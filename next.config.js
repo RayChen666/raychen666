2c775616474880440a5f65434d433e2a7f87dd97
Ray Chen <raychen@Rays-MacBook-Pro.local>
2026-05-25 23:46:10 -0400

    add next.config.js file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/@esbuild',
+    ],
+  },
+}
+
+module.exports = nextConfig
